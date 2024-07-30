4e8859b5323236ba23680606b25c38f3c0725913
Louise Wong <hauying101@gmail.com>
2024-07-30 10:29:21 +0000

    Added bar plot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
+import calendar
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
 df = pd.read_csv('fcc-forum-pageviews.csv')
@@ -12,7 +13,7 @@ lower_quantile = df['value'].quantile(0.025)
 upper_quantile = df['value'].quantile(0.975)
 
 df= df[(df['value'] >= lower_quantile) & (df['value'] <= upper_quantile)]
-df.info()
+
 
 def draw_line_plot():
     fig = plt.figure()
@@ -27,13 +28,25 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df
 
+    df_bar['Month'] = pd.DatetimeIndex(df_bar['date']).month
+    df_bar['Year'] = pd.DatetimeIndex(df_bar['date']).year
+    df_bar.head()
+    df_bar['Month'] = df_bar['Month'].apply(lambda x: calendar.month_name[x])
     # Draw bar plot
+    fig, ax = plt.subplots(figsize=(12, 6))
+    hue_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    ax = sns.barplot(x = 'Year',
+                y = 'value',
+                hue = 'Month',
+                data = df_bar,
+                hue_order = hue_order,
+                errorbar=('ci', 0)
+                )
 
-
-
-
+    ax.set(ylabel='Average Page Views', xlabel='Years')
+    #plt.show()
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
